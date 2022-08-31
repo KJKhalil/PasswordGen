dd1ec86336defee4f5bb275099a7da3f72d8af62
TacTac <110001003+KJKhalil@users.noreply.github.com>
2022-08-31 17:17:50 -0400

    Added prompt for character amount for the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  
+  while (true) {
+
+    var userInput = window.prompt("How many characters do you want in your password? Choose 8 - 128.")
+  
+    if (userInput === null) {
+      return
+    }
+  
+    var passLength = parseInt(userInput)
+  
+    if (isNaN(passLength)) {
+      window.alert("Must choose a number.")
+    } 
+    else if (passLength < 8 || passLength > 128) {
+      window.alert("Character amount must be 8 - 128 characters.")
+    }
+    else {
+      break
+    } 
+  }
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
