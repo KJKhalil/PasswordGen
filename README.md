d2bc8cfaa93921e290c275470c52eb37be759f1d
TacTac <110001003+KJKhalil@users.noreply.github.com>
2022-09-01 22:40:29 -0400

    Added comments to explain functionality of css and javascript. Final commit

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
         -   I added confirm prompts so the user can choose if they want lowercase, uppercase, numbers, and or symbols to be included in their password. I made a variable string for lowercase, uppercase, numbers, and symbols containing all of the acceptable characters for each that can be inputed into the password. Then I used 'if" to make it so that whatever the user selects during the prompts is what gets pushed into the password generation.
 
         -   I added functions and a "for" line to allow the password generator to randomly select characters based on the selected character amount and the selected allowed types of characters the user selected to create a fully randomized password.
+
+        -   Finally, I added comments to the css and javascript explaing what the code does and what code affects what on the webpage and within the webpage for its functionality.
+
+## Project Screenshot
+
+![This is an image of how the webpage looks as it is now.](./assets/images/projectscreenshot.png)
+
+### Github Deployed Link
+
+https://kjkhalil.github.io/PasswordGen/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// These functions in combination with a "for" line down below is what allows the password generator to randomize the characters it is given to work with.
+
 function randomInt(min, max) {
   if (!max) {
     max = min
@@ -14,8 +16,13 @@ function randomItem(list) {
   return list[randomInt(list.length)]
 }
 
+// function generatePassword is the main function of the webpage that gives the generate password button its functionality
+
 function generatePassword() {
   
+  // This "while" code is what creates the prompt after the generate password button is pushed that asks the user how many characters they would like in their password.
+  // If they dont choose anything it will "break" from continuing through with the rest of the code. As well as alert them if they type an invalid number.
+
   while (true) {
 
     var userInput = window.prompt("How many characters do you want in your password? Choose 8 - 128.")
@@ -38,6 +45,9 @@ function generatePassword() {
 
   }
 
+  // These variables and if lines are what create the prompts that allow the user to select wether or not they want lowercase, uppercase, numbers, and or symbols in their password.
+  // As well as defining what characters in each of the above options that the password generator can use to create the password.
+  
   var lowercase = window.confirm("Do you want lowercase letters in your password?")
   var uppercase = window.confirm("Do you want uppercase letters in your password?")
   var numbers = window.confirm("Do you want numbers in your password?")
@@ -67,6 +77,8 @@ function generatePassword() {
   }
 
   var generatedPassword = ""
+  
+  // This "for" line is the line mentioned earlier at the top of this javascript page that help the password generator randomly select characters for the password.
 
   for (var i = 0; i < passLength; i++) {
     var randomList = randomItem(characterList)
